6f54b5ec9bae12c7a6f0da947a4d70b4152a866f
Michael Allan <michaelallan@optushome.com.au>
2014-01-02 19:12:34 +1100

    Re-organise files to enable heartbeat test

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 
 describe("vision heartbeat api", function () {
     describe("when requesting resource '/heartbeat'", function () {
-        it("should respond with a 200 response code", function () {
+        it("should respond with a 200 response code", function (done) {
             request(app).get('/heartbeat')
                 .expect('Content-Type', /json/)
                 .expect(200, done);
