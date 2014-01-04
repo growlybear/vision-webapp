e953b61e5348b592aef6be77dbfbafab21e9e8b3
Michael Allan <michaelallan@optushome.com.au>
2014-01-04 16:57:08 +1100

    Formatting refactor

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -1,9 +1,9 @@
 var app     = require('../app');
 var request = require('supertest');
 
-describe("vision heartbeat api", function () {
+describe("Vision heartbeat api", function () {
     describe("when requesting resource '/heartbeat'", function () {
-        it("should respond with a 200 response code", function (done) {
+        it("should respond with 200", function (done) {
             request(app).get('/heartbeat')
                 .expect('Content-Type', /json/)
                 .expect(200, done);
@@ -11,7 +11,7 @@ describe("vision heartbeat api", function () {
     });
 
     describe("when requesting resource '/missing'", function () {
-        it("should respond with a 404 response code", function (done) {
+        it("should respond with 404", function (done) {
             request(app).get('/missing')
                 .expect('Content-Type', /json/)
                 .expect(404, done);
